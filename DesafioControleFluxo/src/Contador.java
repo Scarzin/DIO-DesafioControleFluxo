5e77d7345242ec17dccd2d5773d376abb8e2b721
Scarzin <sc4rzin@gmail.com>
2024-06-19 18:33:06 -0300

    Desafio completo

---
+++ DesafioControleFluxo/src/Contador.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+import exceptions.ParametrosInvalidosException;
+
+public class Contador {
+    public static void main(String[] args) {
+        try (Scanner terminal = new Scanner(System.in)) {
+            System.out.println("Digite o primeiro parâmetro");
+            int parametroUm = terminal.nextInt();
+            System.out.println("Digite o segundo parâmetro");
+            int parametroDois = terminal.nextInt();
+
+            try {
+                // chamando o método contendo a lógica de contagem
+                contar(parametroUm, parametroDois);
+            } catch (Exception exception) {
+                System.out.println("O segundo parâmetro deve ser maior que o primeiro");
+            }
+        }
+    }
+
+    static void contar(int parametroUm, int parametroDois) throws ParametrosInvalidosException {
+        // realizar o for para imprimir os números com base na variável contagem
+
+        if (parametroUm > parametroDois) {
+            throw new ParametrosInvalidosException();
+        }
+        for (int i = 0; i < parametroDois - parametroUm; i++) {
+            System.out.println(i + 1);
+        }
+    }
+}
